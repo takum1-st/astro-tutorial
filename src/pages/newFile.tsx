30787749519e8044f40979d95215d8dce2732b12
もにょっとしたぷにぷにのなにか <134484570+monyopuni@users.noreply.github.com>
2023-11-23 12:13:23 +0900

    This is a committing message.

---
+++ src/pages/newFile.tsx
@@ -0,0 +1,8 @@
+import BaseLayout from '../layouts/BaseLayout.astro';
+import { pageTitle } from './index.astro';
+
+<Fragment>
+<BaseLayout pageTitle={pageTitle}>
+<h2>私の素晴らしいブログのサブタイトル</h2>
+</BaseLayout>
+</Fragment>;
